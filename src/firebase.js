457177e083a19f3a032531f2532e75eee2b0989a
ruben <lrubenfernandez@unicesar.edu.co>
2026-03-29 02:14:44 -0500

    ahora si

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAGWkPu38LsBd1lRZPqkaLtfMDwEHWhfkQ",
+  authDomain: "keyapp-bc368.firebaseapp.com",
+  projectId: "keyapp-bc368",
+  storageBucket: "keyapp-bc368.firebasestorage.app",
+  messagingSenderId: "591365328111",
+  appId: "1:591365328111:web:8c1f641cf94196cb76df1a"
+}
+
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
